6534e30baabb046e06083df833e6528a13e1a94b
Jesper Balle <jba@codehouse.com>
2018-01-23 22:33:23 +0100

    Add log during download

--- download-dr.js
+++ download-dr.js
@@ -15,14 +15,20 @@ for(var slug of slugs) {
 
     var url = `https://www.dr.dk/mu/programcard/expanded/${slug}`;
     axios.get(url).then(response => {
+        console.log(response);
+        if( !response.data.Data[0]) {
+            throw `could not download ${slug} `;
+        }
         var obj = {
-            name: slug,
+            name: response.data.Data[0].Slug,
             urn: response.data.Data[0].Urn
         };
         return obj;
     }).then(data => {
+        console.log(data);
         var dest = `./downloads/${data.name}.mp4`;
         drvideo(data.urn).pipe(fs.createWriteStream(dest))
     })
+    .then(data => console.log(`Done downloading ${data.name}`))
     .catch(err => console.warn(`Cannot download ${slug} due to ${err}`));
 }
