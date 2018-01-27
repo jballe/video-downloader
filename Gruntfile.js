1c4e40d0bd803423b03af75fa1e16107beebbfcd
Jesper Balle <jba@codehouse.com>
2018-01-27 22:35:42 +0100

    Added some sequences

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,7 +9,7 @@ module.exports = function(grunt) {
                     sections: [
                         {
                             name: 'i-bad',
-                            time: [(3*60 + 57), (5*60 + 45)],
+                            time: [(3*60 + 59), (5*60 + 44)],
                             codecs: ['mp4']
                         },
                         {
@@ -23,6 +23,39 @@ module.exports = function(grunt) {
                             codecs: ['mp4']
                         }
                     ]
+                }
+            },
+            "fra_1": {
+                src: 'downloads/onkel-reje-og-boernenes-brevkasse.mp4',
+                dest: 'downloads/onkel-reje-',
+                options: {
+                    sections: [{
+                        name: "mormor",
+                        time: [(3*60 + 02), (4*60 + 55)],
+                        codecs: ['mp4']
+                    }]
+                }
+            },
+            "fra_11": {
+                src: 'downloads/onkel-reje-og-boernenes-brevkasse-11.mp4',
+                dest: 'downloads/onkel-reje-',
+                options: {
+                    sections: [{
+                        name: "danmarks-heldigste-dreng",
+                        time: [(3*60 + 28), (5*60 + 27)],
+                        codecs: ['mp4']
+                    }]
+                }
+            },
+            "fra_7": {
+                src: 'downloads/onkel-reje-og-boernenes-brevkasse-7.mp4',
+                dest: 'downloads/onkel-reje-',
+                options: {
+                    sections: [{
+                        name: "sangen",
+                        time: [(3*60 + 00), (5*60 + 41)],
+                        codecs: ['mp4']
+                    }]
                 }
             }
         }
--- index.js
+++ index.js
@@ -2,3 +2,4 @@ require('./download-dr');
 require('./download-youtube');
 
 console.log('Done!');
+console.log('Now consider running: npm run slicer')
